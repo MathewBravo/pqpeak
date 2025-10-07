9b993b6402006a51ab92c64509d0d5a293884743
Mathew Bravo <mathewbravo94@gmail.com>
2025-10-06 21:32:20 -0400

    More work to stats

--- pqpeek-core/src/stats.rs
+++ pqpeek-core/src/stats.rs
@@ -1,6 +1,6 @@
 use std::io::Error;
 
-use parquet::{basic::{LogicalType, Repetition, Type}, file::{metadata::KeyValue, reader::FileReader}};
+use parquet::{basic::{LogicalType, Repetition, Type}, file::{reader::FileReader, statistics::Statistics}};
 
 use crate::io::get_reader_from_file;
 
@@ -18,45 +18,77 @@ pub struct ColumnDescriptors{
     name: String,
     phys_type: Type,
     logical_type: Option<LogicalType>,
-    percision: i32,
-    scale: i32,
+    percision: Option<i32>,
+    scale: Option<i32>,
     nullable: bool,
+    nc: Option<u64>
 }
+
 
 pub struct VerboseParquetFileStatistics;
 
-pub fn get_quick_stats(path: &str) -> Result<ParquetFileStatistics, Error>{
+pub fn get_statistics(path: &str) -> Result<ParquetFileStatistics, Error>{
     let reader = get_reader_from_file(path)?;
-    let file_metadata = reader.metadata().file_metadata();
+    let meta = reader.metadata();
     
-    let schema = file_metadata.schema_descr();
+    let schema = meta.file_metadata().schema_descr();
     
-    let mut cd: Vec<ColumnDescriptors> = Vec::new();
+    let mut cds: Vec<ColumnDescriptors> = Vec::new();
     
     for i in 0..schema.num_columns(){
         let col = schema.column(i);
         
         let nullable = col.max_rep_level() > 0 || col.self_type().get_basic_info().repetition() != Repetition::REQUIRED;
         
+        let decimal = match col.physical_type() {
+            Type::FLOAT => true,
+            Type::DOUBLE => true,
+            _ => false,
+        };
+        
 
-        cd.push(
-            ColumnDescriptors { 
+
+        
+        let mut null_count: u64 = 0;
+        let mut no_nulls = true;
+
+        for rg_idx in 0..meta.num_row_groups(){
+            let col_chunk = meta.row_group(rg_idx).column(i);
+            match col_chunk.statistics() {
+                Some(stats) => match stats.null_count_opt() {
+                    Some(n) => null_count += n,
+                    None => { no_nulls = false; }
+                },
+                None => { no_nulls = false; }
+            }
+        }
+        
+        let cd = ColumnDescriptors { 
                 name: col.name().to_string(), 
                 phys_type: col.physical_type(), 
                 logical_type: col.logical_type(), 
-                percision: col.type_precision(), 
-                scale: col.type_scale(), 
+                percision: if decimal{
+                    Some(col.type_precision())
+                }else{
+                    None
+                }, 
+                scale: if decimal{
+                    Some(col.type_scale())
+                }else{
+                    None
+                }, 
                 nullable,
-            }
-        );
-        
+                nc: if no_nulls {Some(null_count)} else {None},
+        };
+
+        cds.push(cd);
     }
 
     let quick_stats = ParquetFileStatistics{
-        version: file_metadata.version(),
-        num_rows: file_metadata.num_rows(),
-        created_by: file_metadata.created_by().map(|s| s.to_string()),
-        col_descriptions: cd,
+        version: meta.file_metadata().version(),
+        num_rows: meta.file_metadata().num_rows(),
+        created_by: meta.file_metadata().created_by().map(|s| s.to_string()),
+        col_descriptions: cds,
     };
     
     Ok(quick_stats)
--- pqpeek-tui/src/main.rs
+++ pqpeek-tui/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    match pqpeek_core::stats::get_quick_stats("../order_items.parquet") {
+    match pqpeek_core::stats::get_statistics("../order_items.parquet") {
         Ok(stats) => println!("{:#?}", stats),
         Err(err) => println!("{}", err),
     }
-    match pqpeek_core::stats::get_quick_stats("../Combined_Flights_2020.parquet") {
+    match pqpeek_core::stats::get_statistics("../Combined_Flights_2020.parquet") {
         Ok(stats) => println!("{:#?}", stats),
         Err(err) => println!("{}", err),
     }
